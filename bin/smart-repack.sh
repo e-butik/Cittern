b0e60c8284df3a17642f933e1dae722e11ccc2f1
Magnus Nordlander <magnus@nordlander.se>
2012-06-26 10:16:20 +0200

    Added error handling and extra output

--- bin/smart-repack.sh
+++ bin/smart-repack.sh
@@ -4,9 +4,18 @@ export GIT="/usr/bin/env git"
 export CITTERN="/usr/bin/env cittern"
 
 $GIT repack -A -d
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
 branches=()
 eval "$($GIT for-each-ref --shell --format='branches+=(%(refname))' refs/heads/)"
 for branch in "${branches[@]}"; do
   shortname=${branch#refs\/heads\/}
+  echo "Unpacking $shortname"
   $CITTERN repo:unpack-nearby $shortname
+
+  if [ $? -ne 0 ]; then
+    exit 1
+  fi
 done
